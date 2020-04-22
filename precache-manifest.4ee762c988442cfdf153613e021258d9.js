36a09b4c788ee644e66d52b0f187e8b32f1247c4
mullet1989 <mullet1989@gmail.com>
2020-04-22 09:46:01 -0400

    Updates

---
+++ precache-manifest.4ee762c988442cfdf153613e021258d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ebdd5c487ea59523d8d28b0b841a452",
+    "url": "/nyrr-head-head/index.html"
+  },
+  {
+    "revision": "e5f057a3955b4f2ed333",
+    "url": "/nyrr-head-head/static/css/2.ab29d089.chunk.css"
+  },
+  {
+    "revision": "175bb135676f74d8e53f",
+    "url": "/nyrr-head-head/static/css/main.bf45f1c0.chunk.css"
+  },
+  {
+    "revision": "e5f057a3955b4f2ed333",
+    "url": "/nyrr-head-head/static/js/2.b39f91f6.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/nyrr-head-head/static/js/2.b39f91f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "175bb135676f74d8e53f",
+    "url": "/nyrr-head-head/static/js/main.887ee7c6.chunk.js"
+  },
+  {
+    "revision": "86d0338b8e17e0cf6212",
+    "url": "/nyrr-head-head/static/js/runtime-main.cea5c31b.js"
+  }
+]);
